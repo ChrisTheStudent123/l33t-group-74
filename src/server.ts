895f6dceb6faa1678c14990e6590447b67681142
Chris <chris.popovits@Student.SL.On.Ca>
2026-03-29 18:24:37 -0400

    rate limiter works properly

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { serve } from "bun";
 import { gameRoute } from "./routes/game.route";
 import { ratingRoute } from "./routes/rating.route";
-import { checkRate } from "./utils/rateLimiter";
+import { rateLimiter } from "./utils/rateLimiter";
+import type { RateLimiterRes } from "rate-limiter-flexible";
 
 // import other routes when finished
 serve({
@@ -11,9 +12,7 @@ serve({
 
     //console.log(req);
     //console.log(server.requestIP(req));
-
     const ip = server.requestIP(req);
-
     if (!ip) {
       return new Response(JSON.stringify({ error: "Malformed address details" }), {
         status: 400,
@@ -21,8 +20,40 @@ serve({
       });
     }
 
-    checkRate(ip.address);
+    const rateRes: Response | undefined = await rateLimiter
+      .consume(ip.address, 1)
+      .then((rateLimitRes) => {
+        return undefined;
+      })
+      .catch((rejRes) => {
+        if (rejRes instanceof Error) {
+          // Redis error here
+          console.error(rejRes);
+          return new Response(JSON.stringify({ error: "Internal server error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          });
+        } else {
+          const secs = String(Math.round(rejRes.msBeforeNext / 1000) || 1);
+          const headers = {
+            "Content-Type": "application/json",
+            "Retry-After": secs,
+            "X-RateLimit-Limit": String(rateLimiter.points),
+            "X-RateLimit-Remaining": String(rejRes.remainingPoints),
+            "X-RateLimit-Reset": String(Math.ceil((Date.now() + rejRes.msBeforeNext) / 1000)),
+          };
+          return new Response(JSON.stringify({ error: "Too many requests" }), {
+            status: 429,
+            headers: headers,
+          });
+        }
+      });
 
+    if (rateRes) {
+      return rateRes;
+    }
+
+    console.log("rateRes", rateRes);
     // Route matching
     if (url.pathname.startsWith("/games")) {
       return gameRoute(req);
